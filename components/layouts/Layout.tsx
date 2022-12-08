ff927216014078cf1166f4f5d7d60537c5a86e11
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-12-07 19:45:45 -0600

    First commit

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,28 @@
+import React, { FC } from 'react'
+import Head from 'next/head';
+
+interface Props {
+    title?: string;
+    children: React.ReactNode
+}
+
+
+export const Layout: FC<Props> = ({ children, title }) => {
+
+    return (
+    <>
+        <Head>
+            <title> {title || 'Pokemon App' } </title>
+            <meta name='author' content='Edgar Joaquin Flores Gonzalez'/>
+            <meta name='description' content={`Información sobre pokemones${title}`}/>
+            <meta name='keywords' content={`XXXX, pokemon, pokeapi, pokedex, ${title}`}/>
+        </Head>
+
+        <main>
+            { children }
+        </main>
+    </>
+    )
+
+
+}
