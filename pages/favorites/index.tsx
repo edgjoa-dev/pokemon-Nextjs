6a8e6d3156a4bffdf582adb704f7a3099cb070ff
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-12-09 13:12:13 -0600

    Diseño pagina pokemon

---
+++ pages/favorites/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Layout } from '../../components/layouts'
+
+const FavoritesPage = () => {
+    return (
+        <Layout
+            title='Pókemons Favoritos'
+        >
+            <h2>
+                FavoritesPage
+            </h2>
+        </Layout>
+    )
+}
+
+export default FavoritesPage
