c09d84c676e50aacb1842723d7782013b6ddf869
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-12-11 22:08:10 -0600

    Configuracion de testing

--- __test__/pages/Home.test.js
+++ __test__/pages/Home.test.js
@@ -1,17 +1,15 @@
 import { render, screen } from '@testing-library/react'
-import HomePage from "pages";
+import HomePage from '../pages/index';
+
 
 describe('Pruebas en el componente HomePage', () => {
-    
-    test('Se debe de mostrar correctamente el HomePage', () => {
+
+    test('Se debe de mostrar correctamente', () => {
         render(<HomePage />)
+        screen.debug()
 
-        const heading = screen.getByRole('heading', {
-            name: /welcome to next\.js!/i,
-        
-            expect(heading).toBeInTheDocument()
-        })
+        // const heading = screen.getByRole('heading', {name: /welcome to next\.js!/i})
+
+        // expect(heading).toBeInTheDocument()
     });
-    
-
-});
+})
