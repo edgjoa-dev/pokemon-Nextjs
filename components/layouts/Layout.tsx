c3edb132b5f6a55ca59b53781f2b551194dbbce0
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2022-12-12 18:47:44 -0600

    feature: Agregar Open Graph para la creacion de metatags de SEO

--- components/layouts/Layout.tsx
+++ components/layouts/Layout.tsx
@@ -17,6 +17,9 @@ export const Layout: FC<Props> = ({ children, title }) => {
             <meta name='author' content='Edgar Joaquin Flores Gonzalez'/>
             <meta name='description' content={`Información sobre pokemones${title}`}/>
             <meta name='keywords' content={`XXXX, pokemon, pokeapi, pokedex, ${title}`}/>
+            <meta property="og:title" content= {`Información sobre pokemones${title}`} />
+            <meta property="og:description" content={`Obten los detalles sobre el tu pokemon favorito: ${title}`} />
+            <meta property="og:image" content="https://localhost:3000/img/banner.png" />
         </Head>
 
         {/* navbar */}
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import {NextPage, GetStaticProps } from 'next'
 import { Layout } from '../components/layouts';
 import { pokeApi } from '../api';
 import { PokemonListResponse, SmallPokemon } from '../interfaces';
-import { Grid } from '@nextui-org/react';
+import { Grid, Image } from '@nextui-org/react';
 import { PokemonCard } from '../components/pokemon';
 
 
@@ -17,6 +17,14 @@ const HomePage: NextPage<Props> = ({pokemons}) => {
 
   return (
     <Layout title={'Pokemon App Web'}>
+
+      <Image
+        src='/img/banner.png'
+        alt='Imagen de banner'
+        width={200}
+        height={150}
+      />
+
       <Grid.Container gap={2} justify='flex-start'>
       {
         pokemons.map( ( pokemon )=>(
